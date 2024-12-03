d41fc7f3221716e249e914140a9d54e0c98bcb56
Timotej Vronč <vronctimotej@gmail.com>
2024-12-03 21:19:55 +0100

    edit of constructor parameters type and added class add_neigbor

--- node.py
+++ node.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 
 class Noda (object):
-    def __init__(self, x, y, infected):
+    def __init__(self, x: 'int', y: 'int', infected: 'bool'):
         self.x = x
         self.y = y
         self.infected = infected
         self.neighbors = []
         self.circle = tk.Canvas.create_oval(x, y, x+10, y+10, fill="red" if infected else "blue")
 
+    def add_neighbor(self, neighbor: 'Noda'):
+        if neighbor not in self.neighbors:
+            self.neighbors.append(neighbor)
+            neighbor.add_neighbor(self)
     
     
