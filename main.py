357752307aa69f5862e31f4eea747786aa8923af
Timotej Vronč <vronctimotej@gmail.com>
2024-12-03 21:43:00 +0100

    added first test node

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import tkinter as tk
+from node import Node
+
 WIDTH = 1040
 HEIGHT = 720
 canvas = tk.Canvas(width=WIDTH, height=HEIGHT)
 canvas.pack()
 
+testNode = Node(canvas, 10, 10, False)
 
-canvas.mainloop()
+if __name__ == '__main__':
+    canvas.mainloop()
