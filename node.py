9b80de14c53685d1247680e7dd35ce01cd17e8e1
Timotej Vronč <vronctimotej@gmail.com>
2024-12-03 21:22:19 +0100

    rename of class to Node

--- node.py
+++ node.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-class Noda (object):
+class Node (object):
     def __init__(self, x: 'int', y: 'int', infected: 'bool'):
         self.x = x
         self.y = y
@@ -8,7 +8,7 @@ class Noda (object):
         self.neighbors = []
         self.circle = tk.Canvas.create_oval(x, y, x+10, y+10, fill="red" if infected else "blue")
 
-    def add_neighbor(self, neighbor: 'Noda'):
+    def add_neighbor(self, neighbor: 'Node'):
         if neighbor not in self.neighbors:
             self.neighbors.append(neighbor)
             neighbor.add_neighbor(self)
