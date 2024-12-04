1267593a6bfa1525853ad66a0dabb1af6c320b7f
Timotej Vronč <vronctimotej@gmail.com>
2024-12-04 09:57:38 +0100

    second test node and test connection

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ canvas = tk.Canvas(width=WIDTH, height=HEIGHT)
 canvas.pack()
 
 testNode = Node(canvas, 10, 10, False)
+secondNode = Node(canvas, 100, 100, True)   
+testNode.createConnection(secondNode)
 
 if __name__ == '__main__':
     canvas.mainloop()
