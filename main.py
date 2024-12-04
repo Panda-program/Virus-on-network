93d0065fe034a20347b78c8009547f5b1fd33345
Mykola Zub <zub1@stud.uniza.sk>
2024-12-04 13:00:02 +0100

    add a button to main

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import tkinter as tk
+
 WIDTH = 1040
 HEIGHT = 720
-canvas = tk.Canvas(width=WIDTH, height=HEIGHT)
-canvas.pack()
+
+root = tk.Tk()
+root.geometry(f"{WIDTH}x{HEIGHT}") 
 
 
-canvas.mainloop()
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
+canvas.grid(row=0, column=0, sticky="nsew")  
+
+
+button = tk.Button(
+    root,
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+button.grid(row=0, column=0)
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+root.mainloop()
+
