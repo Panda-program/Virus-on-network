0d85d8756a8d3da69da7194ecc036f0bcb620c52
Timotej Vronč <vronctimotej@gmail.com>
2024-12-04 18:34:26 +0100

    changed test nodes to map

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import tkinter as tk
-from node import Node, State
+from map import Map
 
 WIDTH = 1040
 HEIGHT = 720
 canvas = tk.Canvas(width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-testNode = Node(canvas, 10, 10, State.SUSCEPTIBLE)
-secondNode = Node(canvas, 100, 100, State.SUSCEPTIBLE)   
-testNode.createConnection(secondNode)
+map = Map(canvas)
+map.createMap(10)
+print("SUPER")
 
 if __name__ == '__main__':
     canvas.mainloop()
