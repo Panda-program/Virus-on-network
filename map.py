284f573ccc2beab553b93036aad57793294bb9c6
Timotej Vronč <vronctimotej@gmail.com>
2024-12-04 18:25:09 +0100

    created method createMap

--- map.py
+++ map.py
@@ -1,7 +1,19 @@
 import random
+from node import Node, State
+
 class Map:
     def __init__(self):
         self.width = 700
         self.height = 700
         self.tileSize = 70
         self.tiles = [[[] for _ in range(10)] for _ in range(10)]
+        
+    def createMap(self, numberOfNodes):
+        for i in range(numberOfNodes):
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+            
+            tileX = x // self.tileSize
+            tileY = y // self.tileSize
+            
+            self.tiles[tileX][tileY].append(Node(x, y, State.SUSCEPTIBLE))
