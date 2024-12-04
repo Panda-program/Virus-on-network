e483d96bd791b2d5573b2d3a483363f215cbc67f
Timotej Vronč <vronctimotej@gmail.com>
2024-12-04 12:23:01 +0100

    tests

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tkinter as tk
-from node import Node
+from node import Node, State
 
 WIDTH = 1040
 HEIGHT = 720
 canvas = tk.Canvas(width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-testNode = Node(canvas, 10, 10, False)
-secondNode = Node(canvas, 100, 100, True)   
+testNode = Node(canvas, 10, 10, State.SUSCEPTIBLE)
+secondNode = Node(canvas, 100, 100, State.SUSCEPTIBLE)   
 testNode.createConnection(secondNode)
 
 if __name__ == '__main__':
