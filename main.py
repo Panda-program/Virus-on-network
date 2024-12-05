92c0e534fdef15dff6f8f1c48202c813b304c265
Timotej Vronč <vronctimotej@gmail.com>
2024-12-05 22:39:40 +0100

    testing of new logic

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ HEIGHT = 720
 canvas = tk.Canvas(width=WIDTH, height=HEIGHT, bg="black")
 canvas.pack()
 
-map = Map(canvas)
-map.createMap(10)
-map.createConnections(200, 2)
+map = Map(canvas, 100)
+map.createConnections(100)
 
 print("done")
 canvas.mainloop()
