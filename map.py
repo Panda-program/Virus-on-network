27fb786d6cd7c4025ae409566e6d76279e30acc2
Timotej Vronč <vronctimotej@gmail.com>
2024-12-05 22:38:24 +0100

    new simpler logic for creating connections based on avgNodeDegree

--- map.py
+++ map.py
@@ -21,6 +21,24 @@ class Map:
         totalConnections = avgNodeDegree * self.numberOfNodes // 2
         currentConnections = 0
         
+        while (currentConnections < totalConnections):
+            node = random.choice(self.nodes)
+            if (node.getDegree() >= avgNodeDegree):
+                continue
+            nearestNode = None
+            nearestDistance = float("inf")
+        
+            for n in self.nodes:
+                if (n != node):
+                    distance = self.getDistance(node, n)
+                    if (distance < nearestDistance and n.getDegree() < avgNodeDegree):
+                        nearestNode = n
+                        nearestDistance = distance
+            if (nearestNode != None):
+                node.createConnection(nearestNode)
+                currentConnections += 1
+        
+        
                 
     def getDistance(self, node1, node2):
         return ((node1.x - node2.x) ** 2 + (node1.y - node2.y) ** 2) ** 0.5
